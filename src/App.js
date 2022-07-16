f5bb0fbee775d3f2f201bfaaee2782df5d318966
shaqdeff <shaquillendunda@gmail.com>
2022-07-16 20:48:54 +0300

    Update books route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './index.css';
+import Navbar from './components/Navbar';
+import Books from './components/Books';
 
 function App() {
-  return <div className="container">Hello World!</div>;
+  const books = [
+    {
+      id: 1,
+      genre: 'Action',
+      title: 'Hunger Games',
+      author: 'Suzanne Collins',
+    },
+  ];
+  return (
+    <div className="container">
+      <Navbar />
+      <Routes>
+        <Route
+          path="/books"
+          element={
+            (
+              <Books
+                genre={books[0].genre}
+                title={books[0].title}
+                author={books[0].author}
+              />
+            )
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
