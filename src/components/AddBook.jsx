d473634a239c99b68d0c98822545c22289bef3dc
shaqdeff <shaquillendunda@gmail.com>
2022-07-21 21:49:06 +0300

    Return state as array

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,14 +1,37 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+/* eslint-disable import/extensions */
+import { addBook } from '../Redux/Books/Books';
+
 function AddBook() {
+  const [state, setState] = useState({
+    title: '',
+    author: '',
+  });
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addBook(state.title, state.author));
+  };
+
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  }
+
+
   return (
     <div>
       <h1>Add New Book</h1>
-      <form>
-        <input type="text" id="title" placeholder="Book title" />
-        <input type="text" id="author" placeholder="Author" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" id="title" placeholder="Book title" value={state.title} />
+        <input type="text" id="author" placeholder="Author" value={state.title} />
         <button type="submit">Add Book</button>
       </form>
     </div>
   );
-}
+};
 
 export default AddBook;
