dc4df0a5bef3f3d5b82e079fc6fa480929fee0f1
shaqdeff <shaquillendunda@gmail.com>
2022-07-21 21:52:28 +0300

    Fix linters

--- src/Redux/Books/Books.js
+++ src/Redux/Books/Books.js
@@ -8,7 +8,7 @@ function booksReducer(state = initialState, action) {
     case ADD_BOOK:
       return [...state, action.book];
     case REMOVE_BOOK:
-      return [...state.filter(book => book.id !== action.id)];
+      return [...state.filter((book) => book.id !== action.id)];
     default:
       return state;
   }
