762f3ace50ea96310ff6b9fcd642bc3005994461
shaqdeff <shaquillendunda@gmail.com>
2022-07-21 18:10:28 +0300

    Fix linter errors

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-import booksReducer from './Books/books';
-import categoriesReducer from './Categories/categories';
+import booksReducer from './Books/books.js';
+import categoriesReducer from './Categories/categories.js';
 
 const rootReducer = configureStore({
   reducer: combineReducers({
