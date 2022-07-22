8cc29093c8a64f0aa8634326d8145d6fc9783991
shaqdeff <shaquillendunda@gmail.com>
2022-07-22 04:18:35 +0300

    Refactor Book.js to connect API

--- src/Redux/Books/Books.js
+++ src/Redux/Books/Books.js
@@ -1,56 +1,55 @@
-import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+
+const FETCH_BOOKS = 'bookstore-cms/src/Redux/Books/fetchBooks';
 
 export const ADD_BOOK = './Books/ADD_BOOK';
 export const REMOVE_BOOK = './Books/REMOVE_BOOK';
+const BASEURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/kN7rR9TgYte9zFAAnguR/books';
 
-export const initialState = {
-  books: [
-    {
-      id: uuidv4(),
-      title: 'Game of Thrones',
-      author: 'George R.R. Martin',
-    },
-    {
-      id: uuidv4(),
-      title: 'Hunger Games',
-      author: 'Suzanne Collins',
-    },
-    {
-      id: uuidv4(),
-      title: 'The Subtle Art of Not Giving a F**k',
-      author: 'Mark Manson',
-    },
-    {
-      id: uuidv4(),
-      title: 'The Power of Now',
-      author: 'Eckhart Tolle',
-    },
-  ],
-};
-/* eslint-disable  default-param-last */
-export default function booksReducer(state = initialState, action) {
+export default function booksReducer(state = [], action) {
   switch (action.type) {
-    case ADD_BOOK: {
-      const booksData = [...state.books, action.payload];
-      return { ...state.books, books: booksData };
+    case ADD_BOOK:
+      return action.payload;
+    case FETCH_BOOKS:
+      return action.payload;
+    case REMOVE_BOOK: {
+      return action.payload;
     }
-
-    case REMOVE_BOOK:
-      return {
-        ...state,
-        books: state.books.filter((book) => book.id !== action.payload.id),
-      };
     default:
       return state;
   }
 }
 
-export const addBook = (title, author) => ({
-  type: ADD_BOOK,
-  payload: { title, author, id: uuidv4() },
-});
+export const fetchBooks = () => async (dispatch) => {
+  try {
+    const response = await axios.get(BASEURL);
+    dispatch({
+      type: FETCH_BOOKS,
+      payload: response.data,
+    });
+    return response.data;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+/* jshint camelcase: true */
+export const addBook = ({
+  title, author, item_id, category,
+}) => async (dispatch) => {
+  try {
+    await axios.post(BASEURL, {
+      title,
+      author,
+      item_id,
+      category,
+    });
+    return dispatch(fetchBooks());
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
 
-export const removeBook = (id) => ({
-  type: REMOVE_BOOK,
-  payload: { id },
-});
+export const removeBook = (id) => async (dispatch) => {
+  await axios.delete(`${BASEURL}/${id}`);
+  dispatch(fetchBooks());
+};
