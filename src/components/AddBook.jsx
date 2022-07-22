20dc90160429545074faea6cff0292f9dcfae4ff
shaqdeff <shaquillendunda@gmail.com>
2022-07-22 04:26:09 +0300

    Correct import path

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/books/Books';
+import { addBook } from '../Redux/Books/Books';
 import './Form.css';
 
 // eslint-disable-line
